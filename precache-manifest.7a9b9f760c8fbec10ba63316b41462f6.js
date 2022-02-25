5badd83d5a86713b035c8073dd438cfe0cc1d19a
Tom Staples <staples.2007@yahoo.co.uk>
2022-02-25 18:39:02 +0000

    Updates

---
+++ precache-manifest.7a9b9f760c8fbec10ba63316b41462f6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8712c64a54b6c09a653898f8e615d6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a4f28cc85ae8d5903693",
+    "url": "/portfolio/static/css/main.f3239943.chunk.css"
+  },
+  {
+    "revision": "e1b60fa05c1afbdef551",
+    "url": "/portfolio/static/js/2.4d41455a.chunk.js"
+  },
+  {
+    "revision": "5ab1baa52e351a54fb487f180027ee45",
+    "url": "/portfolio/static/js/2.4d41455a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f28cc85ae8d5903693",
+    "url": "/portfolio/static/js/main.b1ec1251.chunk.js"
+  },
+  {
+    "revision": "b417b3ee81e3e8f9aad5",
+    "url": "/portfolio/static/js/runtime-main.b649af6b.js"
+  },
+  {
+    "revision": "cfd3dce57f28f5955c90fc5b5385d4b3",
+    "url": "/portfolio/static/media/OpenSansCondensed-Bold.cfd3dce5.ttf"
+  },
+  {
+    "revision": "f72299c4d651ab3866ec980874c2b9c0",
+    "url": "/portfolio/static/media/about.f72299c4.png"
+  },
+  {
+    "revision": "b3e424845f6fecccfe6ec8cd113d6443",
+    "url": "/portfolio/static/media/calorieCounter.b3e42484.png"
+  },
+  {
+    "revision": "45a114c8bc678c35dc61db67a51996b3",
+    "url": "/portfolio/static/media/calorieCounterVid.45a114c8.mp4"
+  },
+  {
+    "revision": "236eabd7a04ed96929cfb96e3034e4e6",
+    "url": "/portfolio/static/media/connect4.236eabd7.png"
+  },
+  {
+    "revision": "429a49f671c166d012d2da2b0ff764c3",
+    "url": "/portfolio/static/media/connect4.429a49f6.mp4"
+  },
+  {
+    "revision": "7c62a6a395186e15b404b6f3b1930e2a",
+    "url": "/portfolio/static/media/memoryMatchVid.7c62a6a3.mp4"
+  },
+  {
+    "revision": "9c5596b5b0fb8523014f2adb2ef0db73",
+    "url": "/portfolio/static/media/portfolio-background.9c5596b5.png"
+  },
+  {
+    "revision": "e9badeb2fbefbe2e1b9da44ff610e3c6",
+    "url": "/portfolio/static/media/profile.e9badeb2.png"
+  },
+  {
+    "revision": "881b16f842fc39bce7e75d5de13874fb",
+    "url": "/portfolio/static/media/starStylists.881b16f8.png"
+  },
+  {
+    "revision": "c8ea9b3b6b1cf03a72222e86dd870aa0",
+    "url": "/portfolio/static/media/starStylistsVid.c8ea9b3b.mp4"
+  },
+  {
+    "revision": "fb972d96dd115dc0743515338c16b521",
+    "url": "/portfolio/static/media/todoList.fb972d96.png"
+  },
+  {
+    "revision": "7ee6327fa17304e2e5eeddf093fb64a5",
+    "url": "/portfolio/static/media/todoListVid.7ee6327f.mp4"
+  }
+]);
