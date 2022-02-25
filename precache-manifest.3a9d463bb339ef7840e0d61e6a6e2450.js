77fbda27b037bec8bb7fe47415849cc4490aa81b
Tom Staples <staples.2007@yahoo.co.uk>
2022-02-25 18:16:45 +0000

    Updates

---
+++ precache-manifest.3a9d463bb339ef7840e0d61e6a6e2450.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45006d1b76731c0cfb562656a4b886f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8ca79964ad3f43a6e92",
+    "url": "/static/css/main.610358ab.chunk.css"
+  },
+  {
+    "revision": "e1b60fa05c1afbdef551",
+    "url": "/static/js/2.4d41455a.chunk.js"
+  },
+  {
+    "revision": "5ab1baa52e351a54fb487f180027ee45",
+    "url": "/static/js/2.4d41455a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8ca79964ad3f43a6e92",
+    "url": "/static/js/main.1b9698c4.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "cfd3dce57f28f5955c90fc5b5385d4b3",
+    "url": "/static/media/OpenSansCondensed-Bold.cfd3dce5.ttf"
+  },
+  {
+    "revision": "f72299c4d651ab3866ec980874c2b9c0",
+    "url": "/static/media/about.f72299c4.png"
+  },
+  {
+    "revision": "b3e424845f6fecccfe6ec8cd113d6443",
+    "url": "/static/media/calorieCounter.b3e42484.png"
+  },
+  {
+    "revision": "45a114c8bc678c35dc61db67a51996b3",
+    "url": "/static/media/calorieCounterVid.45a114c8.mp4"
+  },
+  {
+    "revision": "236eabd7a04ed96929cfb96e3034e4e6",
+    "url": "/static/media/connect4.236eabd7.png"
+  },
+  {
+    "revision": "429a49f671c166d012d2da2b0ff764c3",
+    "url": "/static/media/connect4.429a49f6.mp4"
+  },
+  {
+    "revision": "7c62a6a395186e15b404b6f3b1930e2a",
+    "url": "/static/media/memoryMatchVid.7c62a6a3.mp4"
+  },
+  {
+    "revision": "9c5596b5b0fb8523014f2adb2ef0db73",
+    "url": "/static/media/portfolio-background.9c5596b5.png"
+  },
+  {
+    "revision": "e9badeb2fbefbe2e1b9da44ff610e3c6",
+    "url": "/static/media/profile.e9badeb2.png"
+  },
+  {
+    "revision": "881b16f842fc39bce7e75d5de13874fb",
+    "url": "/static/media/starStylists.881b16f8.png"
+  },
+  {
+    "revision": "c8ea9b3b6b1cf03a72222e86dd870aa0",
+    "url": "/static/media/starStylistsVid.c8ea9b3b.mp4"
+  },
+  {
+    "revision": "fb972d96dd115dc0743515338c16b521",
+    "url": "/static/media/todoList.fb972d96.png"
+  },
+  {
+    "revision": "7ee6327fa17304e2e5eeddf093fb64a5",
+    "url": "/static/media/todoListVid.7ee6327f.mp4"
+  }
+]);
